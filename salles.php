a54d547b42dc86e89c1555a9057c14ed800ec3fd
jojojojojota <33525107+jojojojojota@users.noreply.github.com>
2019-06-11 11:54:34 +0200

    Update salles.php

--- salles.php
+++ salles.php
@@ -11,11 +11,11 @@ if (isset($_SESSION['uti_nom'])){
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<?php include("include\menu.php"); ?>
+	<?php include("include/menu.php"); ?>
 	<div class="main">
 		<h1>Liste des salles disponibles pour <?php echo($_SESSION['uti_nom']);echo(" ".$_SESSION['uti_prenom']); ?></h1>
 		<?php
-		require "script\sqlconnect.php";
+		require "script/sqlconnect.php";
 			$reponse = $bdd->query('SELECT acces.sal_id, salle.sal_nom, salle.sal_desc from acces inner join salle on acces.sal_id = salle.sal_id where uti_id=\''.$_SESSION['uti_id'].'\' ORDER BY sal_nom');
 			if($reponse){
 			while($donnees = $reponse->fetch()){
