318b8d46d2d9c4301c12f493cee62136fce5b772
jojojojojota <33525107+jojojojojota@users.noreply.github.com>
2019-06-12 09:38:15 +0200

    Update sqlconnect.php
    Ajout d'une connection à la db du wapt

--- script/sqlconnect.php
+++ script/sqlconnect.php
@@ -5,4 +5,5 @@ $portdb='5432';
 $userdb='';
 $mdpdb='';
 $bdd = new PDO('pgsql:host='.$hostnamedb.';dbname='.$dbname.';port='.$portdb.';', $userdb, $mdpdb);
+$bddwapt = new PDO('pgsql:host='.$hostnamedb.';dbname=wapt;port='.$portdb.';', $userdb, $mdpdb);
 ?>
